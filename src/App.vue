248338f3b32a4285084552bebaf1bf0ff897da16
jumiray <jumiray@gmail.com>
2022-10-29 19:40:23 +0800

    feat: roma

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,67 @@
 <template>
   <div>
     <h1>哪里不会点哪里</h1>
+    <div class="korean-wrapper">
+      <table border="1">
+        <thead>
+        <tr>
+          <th  v-for="(item, index) in tableHeader" :key="index" @click="play(item.roma,true)">
+            <div class="koren-value">{{item.value}}</div>
+            <div class="korean-roma"> {{item.roma}}</div>
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item, index) in tableContent" :key="index">
+          <td v-for="(i,num) in item" :key="num"  @click="play(Boolean(i.roma)? i.roma : i.value, Boolean(i.roma))">
+            <div class="koren-value">{{i.value }}</div>
+            <div class="korean-roma"> {{Boolean(i.roma) ? i.roma : `${item[0].roma}${tableHeader[num].roma}`}}</div>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <audio :src="audioSrc" id="RomaSound" ></audio>
   </div>
 </template>
 
-<script lang="ts" setup>
-
+<script  setup>
+import {computed, ref} from "vue";
+const nowRoma = ref('')
+const nowValue = ref('')
+const audioSrc = computed(()=> nowRoma.value? `https://www.2hanyu.com/mp3/yinbiao/${nowRoma.value}.mp3`: `https://dict.youdao.com/dictvoice?audio=${nowValue.value}&le=ko`)
+const play = (roma, isRoma) => {
+  console.log(roma, isRoma)
+  nowValue.value = isRoma ? '' : roma
+  nowRoma.value = isRoma ? roma : '';
+  if(!nowRoma.value && !nowValue.value) return
+  setTimeout(()=> {
+    document.getElementById('RomaSound').pause()
+    document.getElementById('RomaSound').play()
+  },100)
+}
+const tableHeader = [{value: '', roma: ''},
+                          {"value":"ㅏ", roma:"a"}, {"value":"ㅐ", roma:"ae"}, {"value":"ㅑ", roma:"ya"}, {"value":"ㅒ", roma:"yae"},{"value":"ㅓ", roma:"eo"},{"value":"ㅔ", roma:"e"}, {"value":"ㅕ", roma:"yeo"}, {"value":"ㅖ", roma:"ye"}, {"value":"ㅗ", roma:"o"}, {"value":"ㅘ", roma:"wa"}, {"value":"ㅙ", roma:"wae"}, {"value":"ㅚ", roma:"oe"}, {"value":"ㅛ", roma:"yo"}, {"value":"ㅜ", roma:"u"}, {"value":"ㅝ", roma:"wo"}, {"value":"ㅞ", roma:"we"}, {"value":"ㅟ", roma:"wi"}, {"value":"ㅠ", roma:"yu"}, {"value":"ㅡ", roma:"eu"}, {"value":"ㅢ", roma:"ui"}, {"value":"ㅣ", roma:"i"}]
+const tableContent = [
+  [{"value":"ㄱ",roma:"g"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㄷ",roma:"d"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅂ",roma:"b"},{value:"바", roma:""},{value:"배", roma:""},{value:"뱌", roma:""},{value:"뱨", roma:""},{value:"버", roma:""},{value:"베", roma:""},{value:"벼", roma:""},{value:"볘", roma:""},{value:"보", roma:""},{value:"봐", roma:""},{value:"봬", roma:""},{value:"뵈", roma:""},{value:"뵤", roma:""},{value:"부", roma:""},{value:"붜", roma:""},{value:"붸", roma:""},{value:"뷔", roma:""},{value:"뷰", roma:""},{value:"브", roma:""},{value:"븨", roma:""},{value:"비", roma:""}],
+  [{"value":"ㅅ",roma:"s"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅈ",roma:"j"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㄲ",roma:"kk"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㄸ",roma:"tt"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅃ",roma:"pp"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅆ",roma:"ss"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅉ",roma:"jj"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅋ",roma:"k"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅌ",roma:"t"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅍ",roma:"p"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅊ",roma:"ch"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㄴ",roma:"n"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㄹ",roma:"l"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅁ",roma:"m"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅇ",roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}],
+  [{"value":"ㅎ",roma:"h"},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""},{value:"", roma:""}]]
 </script>
 
 <style>
@@ -16,5 +72,20 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.korean-wrapper {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  font-size: 40px;
+}
+.koren-value {
+  min-width: 55px;
+}
+.korean-roma {
+  font-size: 20px;
+}
+.korean-wrapper table {
+  cursor: pointer;
 }
 </style>
